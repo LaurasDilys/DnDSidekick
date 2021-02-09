b07465c13687dc34f954c29b7900a9d456f895bd
Lauras Dilys <lauras.dilys@gmail.com>
2021-02-09 18:13:52 +0200

    Add project files.

---
+++ DnDSidekick.Commons/Models/Creature.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDSidekick.Commons.Models
+{
+    public abstract class Creature
+    {
+        public Creature()
+        {
+            
+        }
+
+        public string Name { get; set; }
+
+        public int ProficiencyBonus { get; set; }
+
+        //Ability Scores
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+
+        //Skills
+        //Strength based skills
+        public int StrengthSavingThrow { get; set; }
+        public int Athletics { get; set; }
+
+        //Dexterity based skills
+        public int DexteritySavingThrow { get; set; }
+        public int Acrobatics { get; set; }
+        public int SleightOfHand { get; set; }
+        public int Stealth { get; set; }
+
+        //Constitution based skills
+        public int ConstitutionSavingThrow { get; set; }
+
+        //Intelligence based skills
+        public int IntelligenceSavingThrow { get; set; }
+        public int Arcana { get; set; }
+        public int History { get; set; }
+        public int Investigation { get; set; }
+        public int Nature { get; set; }
+        public int Religion { get; set; }
+
+        //Wisdom based skills
+        public int WisdomSavingThrow { get; set; }
+        public int AnimalHandling { get; set; }
+        public int Insight { get; set; }
+        public int Medicine { get; set; }
+        public int Perception { get; set; }
+        public int Survival { get; set; }
+
+        //Charisma based skills
+        public int CharismaSavingThrow { get; set; }
+        public int Deception { get; set; }
+        public int Intimidation { get; set; }
+        public int Performance { get; set; }
+        public int Persuasion { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ DnDSidekick.Commons/Services/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnDSidekick.Commons.Services
+{
+    public static class ExtensionMethods
+    {
+        public static int Modifier(this int abilityScore) => Convert.ToInt32(Math.Floor(((float)abilityScore - 10) / 2));
+    }
+}
