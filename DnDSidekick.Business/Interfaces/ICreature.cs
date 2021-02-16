48e1c19ad71d9c277f53b5bb5469eafa7e6adb02
Lauras Dilys <lauras.dilys@gmail.com>
2021-02-16 14:04:02 +0200

    ICreature created

---
+++ DnDSidekick.Business/Interfaces/ICreature.cs
@@ -0,0 +1,57 @@
+﻿using DnDSidekick.Commons.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDSidekick.Business.Interfaces
+{
+    public interface ICreature
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        int ArmorClass { get; set; }
+        int HitPoints { get; set; }
+        int ProficiencyBonus { get; set; }
+
+
+        //Abilities
+        Ability Strength { get; set; }
+        Ability Dexterity { get; set; }
+        Ability Constitution { get; set; }
+        Ability Intelligence { get; set; }
+        Ability Wisdom { get; set; }
+        Ability Charisma { get; set; }
+
+
+        //Skills
+        //Strength based
+        Skill Athletics { get; set; }
+
+        //Dexterity based skills
+        Skill Acrobatics { get; set; }
+        Skill SleightOfHand { get; set; }
+        Skill Stealth { get; set; }
+
+        //Intelligence based skills
+        Skill Arcana { get; set; }
+        Skill History { get; set; }
+        Skill Investigation { get; set; }
+        Skill Nature { get; set; }
+        Skill Religion { get; set; }
+
+        //Wisdom based skills
+        Skill AnimalHandling { get; set; }
+        Skill Insight { get; set; }
+        Skill Medicine { get; set; }
+        Skill Perception { get; set; }
+        Skill Survival { get; set; }
+
+        //Charisma based skills
+        Skill Deception { get; set; }
+        Skill Intimidation { get; set; }
+        Skill Performance { get; set; }
+        Skill Persuasion { get; set; }
+    }
+}
--- DnDSidekick.Business/Models/Character.cs
+++ DnDSidekick.Business/Models/Character.cs
@@ -1,4 +1,5 @@
-﻿using DnDSidekick.Commons.Models;
+﻿using DnDSidekick.Business.Interfaces;
+using DnDSidekick.Commons.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,23 +8,97 @@ using System.Threading.Tasks;
 
 namespace DnDSidekick.Business.Models
 {
-    public class Character : Creature
+    public class Character : ICreature
     {
-        public Character() : base(0) { }
+        public Character() : this(0) { }
 
-        public Character(int id) : base(id)
+        public Character(int id)
         {
+            Id = id;
+
+            if (Id == 1)
+            {
+                ProficiencyBonus = 100;
+            }
         }
 
+        public int Id { get; set; }
+        public string Name { get; set; }
         public string Class { get; set; }
         public string Race { get; set; }
-        public string MyProperty { get; set; }
         public string Background { get; set; }
+        public string Alignment { get; set; }
         public int Level { get; set; } = 1;
         public int ExperiencePoints { get; set; }
+        public int ProficiencyBonus { get; set; } = 2;
         public int Inspiration { get; set; }
 
 
+        public int ArmorClass { get; set; }
+        public int TemporaryArmorClass { get; set; }
 
+
+        public int HitPoints { get; set; } = 1;
+        public int TemporaryHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+
+
+        public int Initiative { get; set; }
+        public int CurrentInitiative { get; set; }
+
+
+        public int Speed { get; set; }
+        public int CurrentSpeed { get; set; }
+
+
+        public int UsedHitDice { get; set; }
+
+
+        public bool DeathSaveSuccess1 { get; set; } = false;
+        public bool DeathSaveSuccess2 { get; set; } = false;
+        public bool DeathSaveSuccess3 { get; set; } = false;
+        public bool DeathSaveFailure1 { get; set; } = false;
+        public bool DeathSaveFailure2 { get; set; } = false;
+        public bool DeathSaveFailure3 { get; set; } = false;
+
+
+
+        //Abilities
+        public Ability Strength { get; set; } = new Ability();
+        public Ability Dexterity { get; set; } = new Ability();
+        public Ability Constitution { get; set; } = new Ability();
+        public Ability Intelligence { get; set; } = new Ability();
+        public Ability Wisdom { get; set; } = new Ability();
+        public Ability Charisma { get; set; } = new Ability();
+
+
+        //Skills
+        //Strength based
+        public Skill Athletics { get; set; } = new Skill();
+
+        //Dexterity based skills
+        public Skill Acrobatics { get; set; } = new Skill();
+        public Skill SleightOfHand { get; set; } = new Skill();
+        public Skill Stealth { get; set; } = new Skill();
+
+        //Intelligence based skills
+        public Skill Arcana { get; set; } = new Skill();
+        public Skill History { get; set; } = new Skill();
+        public Skill Investigation { get; set; } = new Skill();
+        public Skill Nature { get; set; } = new Skill();
+        public Skill Religion { get; set; } = new Skill();
+
+        //Wisdom based skills
+        public Skill AnimalHandling { get; set; } = new Skill();
+        public Skill Insight { get; set; } = new Skill();
+        public Skill Medicine { get; set; } = new Skill();
+        public Skill Perception { get; set; } = new Skill();
+        public Skill Survival { get; set; } = new Skill();
+
+        //Charisma based skills
+        public Skill Deception { get; set; } = new Skill();
+        public Skill Intimidation { get; set; } = new Skill();
+        public Skill Performance { get; set; } = new Skill();
+        public Skill Persuasion { get; set; } = new Skill();
     }
 }
